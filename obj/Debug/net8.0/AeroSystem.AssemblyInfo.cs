4f2ca6321b27e8c23a870cf7677803da944d5ae2
nathangtg <22099865@imail.sunway.edu.my>
2024-06-06 23:46:38 +0800

    Made changes to the project assembly

--- obj/Debug/net8.0/AeroSystem.AssemblyInfo.cs
+++ obj/Debug/net8.0/AeroSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AeroSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c0621d9f7aba98b8b729458c87c389f6a715431")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d062d38ebb9565516e557b9fe929736b183f3ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("AeroSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AeroSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
